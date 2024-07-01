e8569fb07be970e7b747ff6bde103e49c85b9577
Abdelrahman Fakhry Hussein <86444013+Abdelrahman-Fakhry-Hussein@users.noreply.github.com>
2024-07-01 06:10:09 +0300

    Solution of ACM TIMUS 1607 Taxi problem
    Solution of ACM TIMUS 1607 Taxi problem

---
+++ ACM TIMUS 1607 Taxi.cpp
@@ -0,0 +1,42 @@
+// ACM  TIMUS 1607 Taxi.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// link of the problem http://acm.timus.ru/problem.aspx?space=1&num=1607
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // link of the problem http://acm.timus.ru/problem.aspx?space=1&num=1607
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    while (a < c)
+    {
+        if (a + b >= c)
+        {
+            cout << c << endl;
+            return 0;
+        }
+        a += b;
+
+        if (c - d <= a)
+        {
+            cout << a << endl;
+            return 0;
+        }
+
+        c -= d;
+    }
+    cout << a << endl;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
